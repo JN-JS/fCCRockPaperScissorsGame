fa2cf6864925813d1ce33782391201407c434274
Jn-html <marmainjean@gmail.com>
2024-10-31 18:14:16 +0100

    determining if player has won the round

--- script.js
+++ script.js
@@ -6,4 +6,22 @@ function getRandomComputerResult() {
 		//console.log(compChoice)
 		return compChoice
   }
-  console.log(getRandomComputerResult());
+// console.log(getRandomComputerResult());
+
+function hasPlayerWonTheRound(player, computer) {
+	// should return true if player won
+	// should return false if player lose or if it's a tie
+	if(player === "Rock" && computer === "Scissors") {
+    return true
+  } else if(player === "Scissors" && computer === "Paper") {
+    return true
+  } else if(player === "Paper" && computer === "Rock") {
+    return true
+  } else {
+    return false
+  }
+}
+
+console.log(hasPlayerWonTheRound("Rock", "Scissors")); 
+console.log(hasPlayerWonTheRound("Scissors", "Rock")); 
+
